47da86e4137692e4140ba4862a39809b2bd53592
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2024-09-26 00:24:55 -0300

    Ejercicio 2 semana 4 terminado

--- Semana_04/AdmEmpleados/AdmEmpleados/obj/Release/net6.0/AdmEmpleados.AssemblyInfo.cs
+++ Semana_04/AdmEmpleados/AdmEmpleados/obj/Release/net6.0/AdmEmpleados.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdmEmpleados")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f941a8eb15facf41029c46d5eaa21afaa4a833d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdmEmpleados")]
---
+++ Semana_04/Registro_postulantes_IFTS/PrimerProyecto/LoginForm.cs
@@ -0,0 +1,35 @@
+﻿using PrimerProyecto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro_postulantes_IFTS
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "Usuario" && txtPass.Text == "Admin123")
+            {
+                Form secondaryForm = new frmPrincipal();
+                secondaryForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario inexistente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
