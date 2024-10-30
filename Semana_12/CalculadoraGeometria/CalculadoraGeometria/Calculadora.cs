332a9174a7c2d2244461d1e6e4cd70f404346361
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2024-10-30 09:19:17 -0300

    Ejercicio S12 clase statica Calculadora funcionando.

---
+++ Semana_12/CalculadoraGeometria/CalculadoraGeometria/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraGeometria
+{
+    internal class Calculadora
+    {
+        public static double Base;
+        public static double Altura;
+        public static double Radio;
+
+        public static double AreaRectangulo()
+        {
+            return (Base * Altura);
+        }
+
+        public static double PerimetroRectangulo()
+        {
+            return ((2 * Base) + (2 * Altura));
+        }
+
+        public static double AreaCirculo()
+        {
+            return (Math.PI * Math.Pow(Radio, 2));
+        }
+
+        public static double PerimetroCirculo()
+        {
+            return (2 * Math.PI * Radio);
+        }
+    }
+}
---
+++ Semana_12/CalculadoraGeometria/CalculadoraGeometria/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculadoraGeometria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora.Radio = 4;
+            Calculadora.Altura = 2;
+            Calculadora.Base = 3;
+
+            Console.WriteLine(Calculadora.AreaRectangulo());
+            Console.WriteLine(Calculadora.AreaCirculo());
+            Console.WriteLine(Calculadora.PerimetroRectangulo());
+            Console.WriteLine(Calculadora.PerimetroCirculo());
+        }
+    }
+}
